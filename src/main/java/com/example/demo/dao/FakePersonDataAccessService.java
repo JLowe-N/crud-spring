49e0885a7a78b6ceb0eec954c5a63033c54303f8
Justin Lowen <Justin.G.Lowen@gmail.com>
2021-01-26 16:09:02 -0600

    Initial POST route setup

---
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -0,0 +1,20 @@
+package com.example.demo.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import com.example.demo.model.Person;
+import org.springframework.stereotype.Repository;
+
+
+@Repository("fakeDao")
+public class FakePersonDataAccessService implements PersonDao {
+    private static List<Person> DB = new ArrayList<>();
+
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        DB.add(new Person(id, person.getName()));
+        return 1;
+    }
+}
